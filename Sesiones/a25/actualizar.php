d6b84d48843f91e98407ad3aeb015521ffec3956
ALUMNADO\bomoca <borjacofrentino@gmail.com>
2017-05-04 11:49:41 +0200

    a25

---
+++ Sesiones/a25/actualizar.php
@@ -0,0 +1,12 @@
+<?php
+  include 'db.php';
+  $usuario= new db();
+  $actualizarperfil=$usuario->actualizarUsuario($_POST['usuario'], $_POST['nombre'], $_POST['apellidos'], $_POST['roles']);
+
+  if ($actualizarperfil==true) {
+    header('Location: MiPerfil.php');
+  }else {
+    echo "Error al actualizar los datos. <br><br>";
+    echo "<a href='MiPerfil.php'>Volver a mi perfil,</a>";
+  }
+ ?>
